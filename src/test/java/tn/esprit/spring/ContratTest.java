3d8f47d0006c643209761f336e0462525a5dc12d
wwwww <www@ww.tn>
2021-11-14 15:33:58 +0100

    commit2

--- src/test/java/tn/esprit/spring/ContratTest.java
+++ src/test/java/tn/esprit/spring/ContratTest.java
@@ -12,7 +12,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.services.IEmployeService;
