cb6fff0b0c1a3876f2ab5ef85bb7790eb187ce15
Mondher HADDAD <mhaddad@naxxum.fr>
2021-11-14 15:47:46 +0100

    Organisation Code

--- src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -29,9 +29,6 @@ public class RestControlEntreprise {
 	@Autowired
 	ITimesheetService itimesheetservice;
 	
-	// Ajouter Entreprise : http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
-	//{"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
-
 	@PostMapping("/ajouterEntreprise")
 	@ResponseBody
 	public int ajouterEntreprise(@RequestBody Entreprise ssiiConsulting) {
@@ -62,7 +59,6 @@ public class RestControlEntreprise {
 	}
     
     // http://localhost:8081/SpringMVC/servlet/ajouterDepartement
- 	//{"id":1,"name":"Telecom"}
 
  	@PostMapping("/ajouterDepartement")
  	@ResponseBody
